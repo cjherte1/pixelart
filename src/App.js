39461817dc420a879be78e280f7984021a0417ca
cjherte1 <92050057+cjherte1@users.noreply.github.com>
2022-01-26 04:07:44 -0600

    added show/hide grid button and functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { HexColorPicker } from "react-colorful";
 
 function App() {
+  const [color, setColor] = useState("#aabbcc");
+  const [isDown, setIsDown] = useState(false);
+  const [hasBorder, setHasBorder] = useState(true);
   var boxes = [];
   for (var i = 0; i < 1296; ++i) {
     boxes.push(
@@ -15,9 +18,6 @@ function App() {
     );
   }
 
-  const [color, setColor] = useState("#aabbcc");
-  const [isDown, setIsDown] = useState(false);
-
   function handleClick(e) {
     e.target.style.backgroundColor = color;
   }
@@ -26,6 +26,13 @@ function App() {
     if (isDown === true) {
       e.target.style.backgroundColor = color;
     }
+  }
+
+  function gridHandler(e) {
+    for (let a of document.getElementsByClassName("MyBox")) {
+      hasBorder ? (a.style.borderWidth = "0") : (a.style.borderWidth = "0.1em");
+    }
+    hasBorder ? setHasBorder(false) : setHasBorder(true);
   }
 
   document.body.addEventListener("mousedown", handleMouseDown);
@@ -43,6 +50,9 @@ function App() {
     <div className="App">
       <div className="Banner"> Pixel Art Maker </div>
       <div className="PickerContainer">
+        <button className="gridButton" onClick={gridHandler}>
+          Show/Hide Grid
+        </button>
         <HexColorPicker color={color} onChange={setColor} />
       </div>
       <div className="Canvas">{boxes}</div>
